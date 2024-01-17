f73b51f1c577ebed8ecb99d68eb4ca1d2f92d321
1622989459@qq.com <1622989459@qq.com>
2024-01-17 11:41:42 +0800

    拆分理念，库，语法

--- Program.cs
+++ Program.cs
@@ -1,11 +1,1 @@
-﻿Console.WriteLine("准备调用异步方法");
-HelloAsync();
-Console.WriteLine("回到调用方法处了");
-
-
-async Task HelloAsync()
-{
-	Console.WriteLine("异步方法开始了");
-	await Task.Yield();
-	Console.WriteLine("异步方法结束了");
-}
+﻿
