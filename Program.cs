1d75fbbe78f314491a3e9bc97bf18a17a9d2cd29
1622989459@qq.com <1622989459@qq.com>
2024-04-18 04:55:27 +0800

    更新

--- Program.cs
+++ Program.cs
@@ -1,28 +1,7 @@
-﻿
+﻿using CO2 = Gas.CarbonDioxide;
+CO2 co2 = new CO2();
 
-
-MyStruct myStruct = new MyStruct("", 12);
-myStruct.Age = 20;//外部只能访问允许访问的东西
-
-struct MyStruct(string name, int age)
+namespace Gas
 {
-	string Name = name;
-	public int Age = age;
-
-	void Hello()
-	{
-		Console.WriteLine(Name);//类型内可以访问成员变量
-	}
-
-	struct Struct2()
-	{
-		void Hello(MyStruct my)
-		{
-			Console.WriteLine(my.Name);//嵌套的类型也可以访问到外部成员。
-		}
-	}
+	class CarbonDioxide;
 }
-
-
-
-
