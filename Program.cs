65dbc9792e339303a43e430d8c9e173556439775
1622989459@qq.com <1622989459@qq.com>
2024-04-17 16:32:38 +0800

    更新

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
 ﻿
+
+
+MyStruct myStruct = new MyStruct("", 12);
+myStruct.Age = 20;//外部只能访问允许访问的东西
+
+struct MyStruct(string name, int age)
+{
+	string Name = name;
+	public int Age = age;
+
+	void Hello()
+	{
+		Console.WriteLine(Name);//类型内可以访问成员变量
+	}
+
+	struct Struct2()
+	{
+		void Hello(MyStruct my)
+		{
+			Console.WriteLine(my.Name);//嵌套的类型也可以访问到外部成员。
+		}
+	}
+}
+
+
+
+
