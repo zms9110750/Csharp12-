9abbca7adb3142d2bcc1ce504ab7cabccf3eacaa
1622989459@qq.com <1622989459@qq.com>
2024-05-06 23:58:05 +0800

    反射

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
-﻿
+﻿using System;
+using System.Buffers;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using System.Reflection;
 
 
-float f = 1e16f;
-Console.WriteLine(f*f);
-Console.WriteLine(f*f*f);
-Console.WriteLine(f*f*f*f);
+
+
+Fight Fight = new Fight() { Atk = 20, Hp = 50 };
+Console.WriteLine(Fight.ToString());
+
+ 
