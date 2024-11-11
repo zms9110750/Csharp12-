039a9f4b2bd5c1d211117de5e2ebd53bd6c4e457
1622989459@qq.com <1622989459@qq.com>
2024-11-12 04:19:02 +0800

    更新语法笔记

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿Console.WriteLine();
+﻿
 
+ 
+foreach (var item in new Test())
+{
+	Console.WriteLine("在foreach循环中输出"+item);
+}
+
+class Test
+{
+	public IEnumerator<int> GetEnumerator()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			Console.WriteLine("在迭代器方法里输出" + i);
+			yield return i;
+		}
+	}
+}
